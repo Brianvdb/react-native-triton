c870472b9841d46ed279e6c0a2a2171f3a6a62b1
Brian <brianvdb11@gmail.com>
2019-02-04 00:14:34 +0100

    javascriptfied the logic

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+import {NativeModules} from 'react-native';
+import {Platform, NativeEventEmitter, DeviceEventEmitter} from 'react-native';
 
-import { NativeModules } from 'react-native';
+const NativeRNTritonPlayer = NativeModules.RNTritonPlayer;
 
-const { RNTritonPlayer } = NativeModules;
+class RNTritonPlayer {
+
+    static play(stream) {
+        NativeRNTritonPlayer.play(stream);
+    }
+
+    static addStreamChangeListener(callback) {
+        if (Platform.OS === 'ios') {
+            const tritonEmitter = new NativeEventEmitter(NativeRNTritonPlayer);
+            tritonEmitter.addListener('streamChanged', callback);
+        } else {
+            DeviceEventEmitter.addListener('streamChanged', callback);
+        }
+    }
+
+    static addTrackChangeListener(callback) {
+        if (Platform.OS === 'ios') {
+            const tritonEmitter = new NativeEventEmitter(NativeRNTritonPlayer);
+            tritonEmitter.addListener('trackChanged', callback);
+        } else {
+            DeviceEventEmitter.addListener('trackChanged', callback);
+        }
+    }
+}
 
 export default RNTritonPlayer;
