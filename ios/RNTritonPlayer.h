925305320c74c13a5f4daa3606d4d8be2fe71ddc
Brian <brianvdb11@gmail.com>
2019-02-03 23:59:26 +0100

    state & events

--- ios/RNTritonPlayer.h
+++ ios/RNTritonPlayer.h
@@ -19,6 +19,17 @@
 #import "TritonPlayerSDK/TritonPlayerSDK.h"
 
 extern NSString* const EventTrackChanged;
+extern NSString* const EventStreamChanged;
+extern NSString* const EventStateChanged;
+
+
+extern const NSInteger STATE_COMPLETED;
+extern const NSInteger STATE_CONNECTING;
+extern const NSInteger STATE_ERROR;
+extern const NSInteger STATE_PLAYING;
+extern const NSInteger STATE_RELEASED;
+extern const NSInteger STATE_STOPPED;
+extern const NSInteger STATE_PAUSED;
 
 @interface RNTritonPlayer : RCTEventEmitter <RCTBridgeModule, TritonPlayerDelegate>
 
--- ios/RNTritonPlayer.m
+++ ios/RNTritonPlayer.m
@@ -2,6 +2,16 @@
 #import "RNTritonPlayer.h"
 
 NSString* const EventTrackChanged = @"trackChanged";
+NSString* const EventStreamChanged = @"streamChanged";
+NSString* const EventStateChanged = @"stateChanged";
+
+const NSInteger STATE_COMPLETED = 200;
+const NSInteger STATE_CONNECTING = 201;
+const NSInteger STATE_ERROR = 202;
+const NSInteger STATE_PLAYING = 203;
+const NSInteger STATE_RELEASED = 204;
+const NSInteger STATE_STOPPED = 205;
+const NSInteger STATE_PAUSED = 206;
 
 @implementation RNTritonPlayer
 
@@ -13,42 +23,79 @@ RCT_EXPORT_MODULE()
 
 - (NSArray<NSString *> *)supportedEvents
 {
-    return @[EventTrackChanged];
+    return @[EventTrackChanged, EventStreamChanged, EventStateChanged];
 }
 
 RCT_EXPORT_METHOD(play:(NSString *)tritonName tritonStation:(NSString *)tritonStation)
 {
+    // Init Triton Player if its not set yet
     if (self.tritonPlayer == NULL) {
         self.tritonPlayer = [[TritonPlayer alloc] initWithDelegate:self andSettings:nil];
     }
     
+    // Set Station Details
     NSDictionary *settings = @{
                                SettingsStationNameKey : tritonName,
                                SettingsBroadcasterKey : @"Triton Digital",
                                SettingsMountKey : tritonStation
                                };
     
+    // Stop Current Stream (if playing)
     if (self.tritonPlayer.state == kTDPlayerStatePlaying) {
         [self.tritonPlayer stop];
     }
     
+    // Update Triton Player settings
     [self.tritonPlayer updateSettings:settings];
     
+    // Start Playing!
     [self.tritonPlayer play];
+    
+    // Notify stream change
+    [self sendEventWithName:EventStreamChanged body:@{@"stream": tritonStation}];
 }
 
 - (void)player:(TritonPlayer *)player didChangeState:(TDPlayerState)state {
+    NSInteger eventState;
     
+    // Map to Android value..
+    switch(state) {
+        case kTDPlayerStateStopped:
+            eventState = STATE_STOPPED;
+            break;
+        case kTDPlayerStatePlaying:
+            eventState = STATE_PLAYING;
+            break;
+        case kTDPlayerStateConnecting:
+            eventState = STATE_CONNECTING;
+            break;
+        case kTDPlayerStatePaused:
+            eventState = STATE_PAUSED;
+            break;
+        case kTDPlayerStateError:
+            eventState = STATE_ERROR;
+            break;
+        case kTDPlayerStateCompleted:
+            eventState = STATE_COMPLETED;
+            break;
+    }
+    
+    // Notify state change
+    [self sendEventWithName:EventStateChanged body:@{@"state": @(eventState)}];
 }
 
 - (void)player:(TritonPlayer *)player didReceiveCuePointEvent:(CuePointEvent *)cuePointEvent {
     if ([cuePointEvent.type isEqualToString:EventTypeAd]) {
+        // Type CUE ad
         [self sendEventWithName:EventTrackChanged body:@{@"artist": @"-", @"title": @"-", @"isAd": @TRUE}];
     } else if ([cuePointEvent.type isEqualToString:EventTypeTrack]) {
+        // Type CUE track
+        
         NSString *songTitle = [cuePointEvent.data objectForKey:CommonCueTitleKey];
         NSString *artistName = [cuePointEvent.data objectForKey:TrackArtistNameKey];
         
         [self sendEventWithName:EventTrackChanged body:@{@"artist": artistName, @"title": songTitle, @"isAd": @FALSE}];
+        
     }
 }
 
