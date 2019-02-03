68e742d0d613fcc6bbe4797b588229adba0fb3b6
Brian <brianvdb11@gmail.com>
2019-02-03 02:35:46 +0100

    maybe this..

--- ios/RNTritonPlayer.m
+++ ios/RNTritonPlayer.m
@@ -6,7 +6,7 @@
 - (dispatch_queue_t)methodQueue
 {
     return dispatch_get_main_queue();
-}
+ }
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(play:(NSString *)tritonName tritonStation:(NSString *)tritonStation)
